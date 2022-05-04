eb334624cf5ae313e91d191f1cb5c6faa0a3d119
Yineth Duarte <yinethpao170@gmail.com>
2022-05-04 16:38:37 -0500

    se quitó registro de landingPages

--- low-price-app/src/index.js
+++ low-price-app/src/index.js
@@ -6,12 +6,11 @@ import { store } from './store/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppRoutes } from './routes/AppRoutes';
 import '../src/styles/index.scss'
-import Map from './components/Map';
+
 
 ReactDOM.render(
   <Provider store={store}>
     <AppRoutes />
-    {/* <Map/> */}
   </Provider>,
   document.getElementById('root')
 );
