7317d025213cb0b8a3d11e07ad45bad5c857afa7
Daniel3132 <danielcorrea3131@gmail.com>
2022-05-05 08:58:24 -0500

    estilos fav

--- low-price-app/src/components/Favoritos.js
+++ low-price-app/src/components/Favoritos.js
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import estaciones from '../data/dataCopy'
 import { useDispatch, useSelector } from 'react-redux'
+import { listAsyn } from '../redux/actions/actionEstaciones'
+
 const Favoritos = () => {
     const navigate = useNavigate()
-    let { lugares } = useSelector(store => store.lugares)
+    const dispatch = useDispatch()
+
+    let { estaciones } = useSelector(store => store.estaciones)
+
+    useEffect(() => {
+        dispatch(listAsyn())
+    }, [])
+
     return (
         <section id='favoritos'>
-			<h1 onClick={()=>navigate('/')} className='lowPriceTitle'>Low Price</h1>
-			<hr />
+            <h1 onClick={() => navigate('/')} className='lowPriceTitle'>Low Price</h1>
+            <hr />
+            <h2>Favoritos</h2>
+            <div className='indicePrecios'>
+                <span className='red'> Corriente </span>
+                <span className='green'> Extra </span>
+                <span className='blue'> ACPM </span>
+            </div>
+            <hr />
             {
                 estaciones.map((s, index) =>
-                    <div key={index}>
-                        <h3>{s.name}</h3>
-                        <h4>${s.precio.gasolinaCorriente}</h4>
-                    </div>
+                    <section key={index}>
+                        <div >
+                            <h3>{s.name}</h3>
+                            <div>
+                                <h4 className='red precios'>${s.precio.gasolinaCorrienteNumero}</h4>
+                                <h4 className='green precios'>${s.precio.gasolinaExtraNumero}</h4>
+                                <h4 className='blue precios'>${s.precio.acpmNumero}</h4>
+                            </div>
+                        </div>
+                        <hr />
+                    </section>
                 )
             }
         </section>
