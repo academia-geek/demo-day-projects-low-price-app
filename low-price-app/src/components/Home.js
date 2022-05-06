18d81204e2b54394a891f8dda910ed1b32c5b031
Daniel3132 <danielcorrea3131@gmail.com>
2022-05-06 00:06:22 -0500

    home responsive

--- low-price-app/src/components/Home.js
+++ low-price-app/src/components/Home.js
@@ -25,22 +25,26 @@ const Home = () => {
       function (error) {
         console.log(error)
       },
-      { enableHighAccuracy: true, maximumAge:0 }
+      { enableHighAccuracy: true, maximumAge: 0 }
     );
   }, []);
-  
-  
+
+
   return (
     <section className='homeTextCont'>
       <h1>Bienvenido a Low Price App</h1>
-      <img alt='' src={logo}/>
-      <small>Recuerda que para tener una mejor experiencia de usuario,  debes darnos permisos para acceder a tu ubicacion; asi sabremos en donde estas y podremos mostrarte las estaciones cercanas</small>
+      <div>
+        <img alt='' src={logo} />
+        <small>Recuerda que para tener una mejor experiencia de usuario,  debes darnos permisos para acceder a tu ubicacion; asi sabremos en donde estas y podremos mostrarte las estaciones cercanas</small>
+      </div>
       <hr />
-      <p>El tiempo y el dinero que gastamos en dirigirnos a una estacion de gasolina y no encontrar el producto indicado o el precio justo, pueden ser situaciones molestas para quien tiene un vehiculo. Low Price App es una aplicacion que busca hacerte la vida mas comoda, ya que te proporciona las estaciones cercanas a tu ubicacion, algunos de los productos que ofrecen  y su precio, para que seas tú quien decida cual es la mejor alternativa.</p>
-      <p>Somos una aplicacion que piensa en ti y en tu economía. </p>
-      <Link to={{
-        pathname: '/map',
-      }}>Ver mi Geolocation</Link>
+      <p>El tiempo y el dinero que gastamos en dirigirnos a una estacion de gasolina y no encontrar el producto indicado o el precio justo, pueden ser situaciones molestas para quien tiene un vehiculo. 
+       </p><p> Low Price App es una aplicacion que busca hacerte la vida mas comoda, ya que te proporciona las estaciones cercanas a tu ubicacion, algunos de los productos que ofrecen  y su precio, para que seas tú quien decida cual es la mejor alternativa.</p>
+      <strong>Somos una aplicacion que piensa en ti y en tu economía. </strong>
+      <button>
+        <Link to={{
+          pathname: '/map',
+        }}>Ver mi Ubicación</Link></button>
     </section>
   )
 }
